5fb7c34be04e48366fca672558697a1bf979296d
Jeffrey S. Racine <racinej@mcmaster.ca>
2024-07-30 22:00:44 -0400

    I believe I have cut down on unnecessary computation by checking for repeated x values

--- R/bkcde.R
+++ R/bkcde.R
@@ -370,28 +370,30 @@ bkcde.default <- function(h=NULL,
       int.f.seq.pre.neg <- numeric()
       int.f.seq <- numeric()
       int.f.seq.post <- numeric()
-      for(j in 1:length(x.eval)) {
-        K <- kernel.bk(x.eval[j],x,h[2],x.lb,x.ub)
+      ## Here we try to be cute as well, and test for unique values 
+      x.eval.unique <- unique(x.eval)
+      for(j in 1:length(x.eval.unique)) {
+        K <- kernel.bk(x.eval.unique[j],x,h[2],x.lb,x.ub)
         if(degree == 0) {
           f.seq <- as.numeric(mcmapply(function(i){mean(kernel.bk(y.seq[i],y,h[1],y.lb,y.ub)*K)/NZD(mean(K))},1:n.integrate,mc.cores=ksum.cores))
         } else {
           X.poly <- poly(x,raw=poly.raw,degree=degree)
           X <- cbind(1,X.poly)
-          X.eval <- cbind(1,predict(X.poly,x.eval[j]))
+          X.eval <- cbind(1,predict(X.poly,x.eval.unique[j]))
           f.seq <- as.numeric(mcmapply(function(i){beta.hat<-coef(lm.wfit(x=X,y=kernel.bk(y.seq[i],y,h[1],y.lb,y.ub),w=NZD(K)));beta.hat[!is.na(beta.hat)]%*%t(X.eval[,!is.na(beta.hat),drop = FALSE])},1:n.integrate,mc.cores=ksum.cores))
         }
         ## Ensure the final result is proper (i.e., non-negative and integrates
         ## to 1)
-        if(verbose & f.yx[j] < 0) warning("negative density estimate reset to 0 via option proper=TRUE in bkcde() [degree = ",
-                                            degree,
-                                            ", j = ",
-                                            j,
-                                            " element(s), h.y = ",
-                                            round(h[1],5),
-                                            ", h.x = ",
-                                            round(h[2],5),
-                                            "]",
-                                            immediate. = TRUE)
+        if(verbose & any(f.yx[x.eval==x.eval.unique[j]] < 0)) warning("negative density estimate reset to 0 via option proper=TRUE in bkcde() [degree = ",
+                                                                      degree,
+                                                                      ", j = ",
+                                                                      length(x.eval==x.eval.unique[j]),
+                                                                      " element(s), h.y = ",
+                                                                      round(h[1],5),
+                                                                      ", h.x = ",
+                                                                      round(h[2],5),
+                                                                      "]",
+                                                                      immediate. = TRUE)
         ## Compute integral of f.seq including any possible negative values
         int.f.seq.pre.neg[j]<- integrate.trapezoidal(y.seq,f.seq)[length(y.seq)]
         ## Set any possible negative f.seq values to 0
@@ -402,8 +404,10 @@ bkcde.default <- function(h=NULL,
         ## and correcting to ensure final estimate integrates to 1
         int.f.seq.post[j] <- integrate.trapezoidal(y.seq,f.seq/int.f.seq[j])[length(y.seq)]
         # Correct the estimate to ensure it is non-negative and integrates to 1
-        if(f.yx[j] < 0) f.yx[j] <- 0
-        f.yx[j] <- f.yx[j]/int.f.seq[j]      
+        foo <- f.yx[x.eval==x.eval.unique[j]]
+        foo[foo<0] <- 0
+        foo <- foo/int.f.seq[j]     
+        f.yx[x.eval==x.eval.unique[j]] <- foo
       }
       ## As a summary measure report the mean of the integrals
       int.f.seq.pre.neg <- mean(int.f.seq.pre.neg)
