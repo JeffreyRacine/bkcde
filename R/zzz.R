7a2cc7a6399f05ec583120c32c71c2a3e553910d
Jeffrey S. Racine <racinej@mcmaster.ca>
2024-08-01 20:14:25 -0400

    Now with new plot functionality, bump version number up to 1.6

--- R/zzz.R
+++ R/zzz.R
@@ -1,3 +1,3 @@
 .onAttach <- function (lib, pkg) {
-	packageStartupMessage("package bkcde version 1.5", domain = NULL,  appendLF = TRUE)
+	packageStartupMessage("package bkcde version 1.6", domain = NULL,  appendLF = TRUE)
 }
