2a3081f7b8bd6b1d9d69f7195550281b39170293
Jeffrey S. Racine <racinej@mcmaster.ca>
2024-08-11 10:59:19 -0400

    Cleaned up patent demo further

--- demo/patent_world.R
+++ demo/patent_world.R
@@ -1,3 +1,6 @@
+library(bkcde)
+suppressPackageStartupMessages(library(np))
+options(np.messages=FALSE)
 suppressPackageStartupMessages(library(pglm))
 ## Patent data often has a preponderance of zeros and "overdispersion". Since
 ## the data has a boundary at zero, a zero-inflated negative binomial model is
@@ -7,25 +10,22 @@ suppressPackageStartupMessages(library(pglm))
 data(PatentsRD)
 attach(PatentsRD)
 
-library(bkcde)
-library(np)
-
 x <- as.integer(as.character(year))
 y <- patent
 
 bkcde.out <- bkcde(x=x,y=y)
 
-summary(bkcde.out)
-
+## Estimator of Hall, Racine & Li (polynomial order 0 conditional density estimate)
 f.np <- npcdens(txdat=x,exdat=rep(1987,100),tydat=y,eydat=seq(min(y),100,length=100))
 
-## The data has a very long tail so we hone in on firms having 100 or fewer
-## patents/year. We first take univariate histogram and density() estimators
-## for the year 1987, then overlay the conditional kernel density estimates from
-## npcdens() and bkcde() for the same year (these use data for all years). We
-## also plot the bkcde() 2D estimates for the year 1987 without, then with
-## simultaneous confidence intervals. Finally, we plot the bkcde() estimate in
-## 3D with and without confidence intervals (all years
+## In the following plots note that the data has a very long tail (right skewed)
+## so we hone in on firms having 100 or fewer patents/year. We first take
+## univariate histogram and density() estimators for the year 1987, then overlay
+## the conditional kernel density estimates from npcdens() and bkcde() for the
+## same year (these use data for all years). We also plot the bkcde() 2D
+## estimates for the year 1987 without, then with simultaneous confidence
+## intervals. Finally, we plot the bkcde() estimate in 3D with and without
+## confidence intervals (all years
 
 par(mfrow=c(2,2),cex=.7)
 
@@ -36,5 +36,7 @@ lines(seq(min(y),100,length=100),predict(bkcde.out,newdata=data.frame(x=rep(1987
 legend("topright",legend=c("density() kernel density","npcdens() conditional kernel","bckde() conditional kernel"),col=1:3,lty=1:3,lwd=c(1,2,3),bty="n")
 
 plot(bkcde.out,plot.3D=FALSE,x.eval=1987,plot.2D.y.grid=seq(min(y),100,length=100),xlim=c(0,100))
+
 plot(bkcde.out,plot.3D=FALSE,x.eval=1987,plot.2D.y.grid=seq(min(y),100,length=100),xlim=c(0,100),ci=TRUE,ci.method="Simultaneous",ci.preplot=FALSE,B=1000)
+
 plot(bkcde.out,plot.3D=TRUE,plot.3D.x.grid=seq(min(x),max(x),length=25),plot.3D.y.grid=seq(min(y),100,length=25),proper=FALSE,ci=TRUE,ci.method="Simultaneous",ci.preplot=FALSE,B=1000)
