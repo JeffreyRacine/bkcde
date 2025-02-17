7549179c504536399f900b6c2239b2c32b6271cd
Jeffrey S. Racine <racinej@mcmaster.ca>
2025-02-17 11:36:09 -0500

    Using extendrange(,f=...) as passable parameters when bounds are infinite

--- R/bkcde.R
+++ R/bkcde.R
@@ -360,6 +360,7 @@ bkcde.default <- function(h=NULL,
                           degree.min=0,
                           degree=NULL,
                           fitted.cores=detectCores(),
+                          integrate.erf=1,
                           n.grid=25,
                           n.integrate=100,
                           n.sub=300,
@@ -642,9 +643,9 @@ bkcde.default <- function(h=NULL,
       if(progress) cat("\rComputing integrals to ensure estimate is proper...\n",sep="")
       ## Create a sequence of values along an appropriate grid to compute the integral.
       if(is.finite(y.lb) && is.finite(y.ub)) y.seq <- seq(y.lb,y.ub,length=n.integrate)
-      if(is.finite(y.lb) && !is.finite(y.ub)) y.seq <- seq(y.lb,extendrange(y,f=2)[2],length=n.integrate)
-      if(!is.finite(y.lb) && is.finite(y.ub)) y.seq <- seq(extendrange(y,f=2)[1],y.ub,length=n.integrate)
-      if(!is.finite(y.lb) && !is.finite(y.ub)) y.seq <- seq(extendrange(y,f=2)[1],extendrange(y,f=2)[2],length=n.integrate)
+      if(is.finite(y.lb) && !is.finite(y.ub)) y.seq <- seq(y.lb,extendrange(y,ferf.integrate)[2],length=n.integrate)
+      if(!is.finite(y.lb) && is.finite(y.ub)) y.seq <- seq(extendrange(y,f=integrate.erf)[1],y.ub,length=n.integrate)
+      if(!is.finite(y.lb) && !is.finite(y.ub)) y.seq <- seq(extendrange(y,f=integrate.erf)[1],extendrange(y,f=integrate.erf)[2],length=n.integrate)
       ## Note that we have a conditional density f(y|x) with potentially repeated
       ## x values, so for each unique x in x.eval we ensure f(y|x) is proper
       ## (avoid unnecessary computation, particularly when x.eval contains a
